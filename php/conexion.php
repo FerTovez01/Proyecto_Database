1480fcaa6ac4e3097d3a3dcc7ac00d2782d7ef9b
FerTovez01 <140423072+FerTovez01@users.noreply.github.com>
2024-11-28 20:05:23 -0600

    CInicial

---
+++ php/conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$server = "localhost";
+$user = "root";
+$pass="";
+$DB="inventariodb";
+
+$conexion = new mysqli ($server, $user,$pass,$DB);
+
+
+if ($conexion -> connect_errno){
+
+     die ("Conexion Fallida". $conexion-> connect_errno);
+
+
+} else {
+
+} 
+?>
